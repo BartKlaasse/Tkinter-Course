4e6bf7203d2c5f82bdbf446b7353c7d69df3c098
Bart Klaasse <bart@bartklaasse.nl>
2020-11-10 22:18:14 +0100

    end of buttons

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -2,7 +2,14 @@ import tkinter as tk
 # make the root widget
 root = tk.Tk()
 
-myButton = tk.Button(root, text="click me")
+
+def myClick():
+    myLabel = tk.Label(root, text="hey i clicked")
+    myLabel.pack()
+
+
+myButton = tk.Button(root, text="click me", padx="50",
+                     pady="50", state="active", command=myClick, fg="blue", bg="red")
 myButton.pack()
 
 # event loop
